1799a331f18b583f6d9297e428b509ce55ce3895
maxmartinezruts <38012876+maxmartinezruts@users.noreply.github.com>
2020-11-21 09:41:15 +0100

    Uploaded Unity project

---
+++ OrbitalMechanics/Assets/Scripts/Body.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Body : MonoBehaviour
+{
+
+    public float mass;
+    
+    public Vector3 vel_ini;
+
+    public List<Vector3> trajectory;
+
+    Vector3 virtualPos;
+    Vector3 virtualVel;
+    Vector3 vel;
+
+    public Universe universe;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vel = vel_ini;
+        ResetVirtual();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateVirtualVel(Body[] bodies, float deltaTime){
+        foreach (var otherBody in bodies)
+        {
+            if (otherBody != this){
+                float dist = Vector3.Distance(virtualPos, otherBody.virtualPos);
+                Vector3 forceDir = Vector3.Normalize(otherBody.virtualPos - virtualPos);
+                Vector3 force = forceDir *  universe.G * mass * otherBody.mass / Mathf.Pow(dist,2);
+                Vector3 acceleration = force/mass;
+                virtualVel += acceleration * deltaTime;
+            }
+        }
+    }
+
+      public void UpdateVel(Body[] bodies, float deltaTime){
+        foreach (var otherBody in bodies)
+        {
+            if (otherBody != this){
+                float dist = Vector3.Distance(transform.position, otherBody.transform.position);
+                Vector3 forceDir = Vector3.Normalize(otherBody.transform.position - transform.position);
+                Vector3 force = forceDir *  universe.G * mass * otherBody.mass / Mathf.Pow(dist,2);
+                Vector3 acceleration = force/mass;
+                vel += acceleration * deltaTime;
+            }
+        }
+    }
+
+    public void UpdateVirtualPos(float deltaTime){
+        virtualPos += virtualVel * deltaTime;
+        trajectory.Add(virtualPos);
+    }
+
+    public void ResetVirtual(){
+        virtualPos = transform.position;
+        virtualVel = vel_ini;
+        trajectory = new List<Vector3>();
+
+    }
+     public void UpdatePos( float deltaTime){
+        transform.position += vel * deltaTime;
+        
+    }
+
+    public void updateTrajectory(){
+        GetComponent<LineRenderer>().positionCount = trajectory.Count;              // Number of points of the line that defines the path
+        for (int i =0; i< trajectory.Count; i++){
+            GetComponent<LineRenderer>().SetPosition(i, trajectory[i]);    // Create a new pivot in line renderer         
+        }   
+   
+    }
+}
---
+++ OrbitalMechanics/Assets/Scripts/Universe.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Universe : MonoBehaviour
+{
+    public float G = 1f;
+    public float deltaT;
+    public int iterations;
+    public Body[] bodies;
+
+    public Slider sliderG;
+    public Text textG;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        G = sliderG.value;
+        textG.text = G.ToString();
+
+        
+        for (int i = 0; i < iterations; i++)
+        {
+            foreach (var body in bodies)
+            {
+                 body.UpdateVirtualVel(bodies, deltaT);   
+            }
+            
+            foreach (var body in bodies)
+            {
+                body.UpdateVirtualPos(deltaT);  
+            }
+        }
+        foreach (var body in bodies)
+        {
+            body.updateTrajectory();
+            body.ResetVirtual();
+        }
+    }
+}
